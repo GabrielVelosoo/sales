b39cd0048baeefcb470400794068533048c9ff83
Gabriel Veloso <gaabrielvelooso@gmail.com>
2025-02-14 15:53:05 -0300

    Project test

--- src/main/java/io/github/gabrielvelosoo/sales/Application.java
+++ src/main/java/io/github/gabrielvelosoo/sales/Application.java
@@ -1,13 +1,22 @@
 package io.github.gabrielvelosoo.sales;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplicationBuilder builder = new SpringApplicationBuilder(Application.class);
+        builder.bannerMode(Banner.Mode.OFF);
+        builder.run(args);
+        ConfigurableApplicationContext applicationContext = builder.context();
+        ConfigurableEnvironment environment = applicationContext.getEnvironment();
+        String applicationName = environment.getProperty("spring.application.name");
+        System.out.println("Application name: " + applicationName);
     }
 
 }
